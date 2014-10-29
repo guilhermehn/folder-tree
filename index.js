c8bbbf41baa1991f2d57b4f09bc64a406a15088b
Guilherme Nagatomo <gnagotomo@sp.r7.com>
2014-10-29 13:50:22 -0200

    jscs

--- index.js
+++ index.js
@@ -1,78 +1,78 @@
-var fs = require('fs')
-  , path = require('path')
-  , _ = require('lodash')
-  , defaults = require('./conf.json')
+var fs = require('fs');
+var path = require('path');
+var _ = require('lodash');
+var defaults = require('./conf.json');
 
-function FolderTree (root, _conf) {
-  this.conf = typeof _conf !== 'undefined' ? _conf : defaults
+function FolderTree (root, userConf) {
+  this.conf = typeof userConf !== 'undefined' ? userConf : defaults;
 
-  this.path = path.resolve(root)
-  this.files = []
-  this.folders = []
+  this.path = path.resolve(root);
+  this.files = [];
+  this.folders = [];
 
   // Initialize folder tree
-  this.readContents()
+  this.readContents();
 }
 
 FolderTree.prototype.readContents = function () {
-  var parent = this
-    , root = this.path
-    , contents = fs.readdirSync(root)
-    , paths
-    , folders
-    , allowedFolders
-    , files
+  var self = this;
+  var root = this.path;
+  var contents = fs.readdirSync(root);
+  var paths;
+  var folders;
+  var allowedFolders;
+  var files;
 
   paths = contents.map(function join (f) {
-    return path.join(root, f)
-  })
+    return path.join(root, f);
+  });
 
   // filter for directories
   folders = paths.filter(function pathsFilter (item) {
-    return fs.statSync(item).isDirectory()
-  })
+    return fs.statSync(item).isDirectory();
+  });
 
   // filter for allowed folders
-  if (parent.conf.ignore_folders) {
+  if (self.conf.ignore_folders) {
     allowedFolders = folders.filter(function foldersFilter (folder) {
-      return parent.conf.ignore_folders.indexOf(path.basename(folder)) === -1
-    })
+      return self.conf.ignore_folders.indexOf(path.basename(folder)) === -1;
+    });
   }
 
   // Filter files
   files = paths
     .filter(function filesFilter (item) {
-      return fs.statSync(item).isFile()
+      return fs.statSync(item).isFile();
     })
-    .map(path.basename) // use only basename
+    .map(path.basename); // use only basename
 
   // ignore files by pattern
-  if (parent.conf.ignore_file_patterns) {
-    parent.conf.ignore_file_patterns.forEach(function eachPattern (pattern) {
+  if (self.conf.ignore_file_patterns) {
+    self.conf.ignore_file_patterns.forEach(function eachPattern (pattern) {
       files = files.filter(function filesPatternFilter (file) {
-        return !file.match(new RegExp(pattern))
-      })
-    })
+        return !file.match(new RegExp(pattern));
+      });
+    });
   }
 
   // store files
-  this.files = files
+  this.files = files;
 
   // store folders
   this.folders = (allowedFolders ? allowedFolders : folders).map(function allowedFoldersMap (folder) {
-    var subfolder = new FolderTree(folder, parent.conf)
-    subfolder.parent = parent
+    var subfolder = new FolderTree(folder, self.conf);
+    subfolder.parent = self;
 
-    return subfolder
-  })
-}
+    return subfolder;
+  });
+};
 
 FolderTree.prototype.updateConf = function (newConf) {
   Object
     .keys(newConf)
     .forEach(function eachKey (key) {
-      this.conf[key] = newConf[key]
-    }.bind(this))
-}
+      this.conf[key] = newConf[key];
+    }.bind(this));
+};
 
-module.exports = FolderTree
+module.exports = FolderTree;
--- tests/FolderTree.specs.js
+++ tests/FolderTree.specs.js
@@ -1,34 +1,38 @@
-var expect = require('chai').expect
-  , FolderTree = require('..')
+var expect = require('chai').expect;
+var FolderTree = require('..');
 
 describe('FolderTree', function () {
-  var tree = new FolderTree('.')
+  var tree = new FolderTree('.');
 
   it('should identify contained files', function () {
-    expect(tree.files.length).to.be.equal(5)
-  })
+    expect(tree.files.length).to.be.equal(5);
+  });
 
   it('should identify contained folders', function () {
-    expect(tree.folders.length).to.be.equal(1)
-  })
+    expect(tree.folders.length).to.be.equal(1);
+  });
 
   it('should maintain a reference to the parent folder for each subdirectory', function () {
-    expect(tree.folders[0].parent.path).to.be.equal(tree.path)
-  })
+    expect(tree.folders[0].parent.path).to.be.equal(tree.path);
+  });
 
   it('should accept a options object as argument', function () {
-    var tree = new FolderTree('.', { ignore_file_patterns: ['.*\.json$'] })
+    var tree = new FolderTree('.', {
+      ignore_file_patterns: ['.*\\.json$']
+    });
 
-    expect(tree.files.length).to.be.equal(3)
-  })
+    expect(tree.files.length).to.be.equal(3);
+  });
 
   describe('#updateConf(object)', function () {
     it('should update the configuration object from the instance', function () {
-      var tree = new FolderTree('.')
-      tree.updateConf({ ignore_file_patterns: ['.*\.json$'] })
-      tree.readContents()
+      var tree = new FolderTree('.');
+      tree.updateConf({
+        ignore_file_patterns: ['.*\\.json$']
+      });
+      tree.readContents();
 
-      expect(tree.files.length).to.be.equal(3)
-    })
-  })
-})
+      expect(tree.files.length).to.be.equal(3);
+    });
+  });
+});
