458d1a03630dc0751072296e323847f8849c42f4
Guilherme Nagatomo <gnagotomo@sp.r7.com>
2014-10-29 13:36:31 -0200

    gulp tasks created

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp');
+var plugins = require('gulp-load-plugins')();
+
+gulp.task('default', function () {
+  return gulp.src('index.js')
+    .pipe(plugins.jscs('.jscsrc'))
+    .pipe(plugins.jshint())
+    .pipe(plugins.jshint.reporter('jshint-stylish'));
+});
+
+gulp.task('watch', function () {
+  gulp.start('default');
+
+  gulp.watch('index.js', ['default']);
+});
