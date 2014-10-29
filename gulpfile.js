4dd63a1c450208d5717df705bbebac0d424dc2be
Guilherme Nagatomo <gnagotomo@sp.r7.com>
2014-10-29 13:50:09 -0200

    check test files

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var plugins = require('gulp-load-plugins')();
 
 gulp.task('default', function () {
-  return gulp.src('index.js')
+  return gulp.src(['*.js', 'tests/*.js'])
     .pipe(plugins.jscs('.jscsrc'))
     .pipe(plugins.jshint())
     .pipe(plugins.jshint.reporter('jshint-stylish'));
